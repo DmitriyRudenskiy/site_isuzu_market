842bb287909f6eb5d72b49c3e5d9b9d1888a25f0
Dmitriy Rudenskiy <dmitriy.rudenskiy@gmail.com>
2017-11-27 15:56:58 +0700

    bug: form

--- app/Http/Controllers/Front/LeasingController.php
+++ app/Http/Controllers/Front/LeasingController.php
@@ -16,6 +16,7 @@ class LeasingController extends Controller
         return view(
             'front.leasing.index',
             [
+                'price' => $product->price,
                 'product' => $product
             ]
         );
