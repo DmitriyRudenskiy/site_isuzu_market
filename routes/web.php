a6bed1239f44ca2c3b7e425b28b72c061f093e72
Dmitriy Rudenskiy <dmitriy.rudenskiy@gmail.com>
2018-03-14 11:55:24 +0700

    fix: for new site

--- routes/web.php
+++ routes/web.php
@@ -3,46 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::group(['middleware' => ['web'], 'namespace' => 'Front', 'as' => 'front_'], function () {
-    Route::get('/', 'DashboardController@home')->name('home');
-    Route::get('/about', 'DashboardController@about')->name('dashboard_about');
-    Route::get('/contacts', 'DashboardController@contacts')->name('dashboard_contacts');
-    Route::get('/service', 'DashboardController@service')->name('dashboard_service');
-    Route::get('/parts', 'DashboardController@parts')->name('dashboard_parts');
-    Route::get('/finance', 'DashboardController@finance')->name('dashboard_finance');
-    Route::get('/vacancy', 'DashboardController@vacancy')->name('dashboard_vacancy');
-
-    Route::get('/leasing/info', 'LeasingController@index')->name('leasing_info');
-    Route::get('/leasing/calculation/{price}', 'LeasingController@calculation')->name('leasing_calculation');
-    Route::get('/leasing/view/{id}', 'LeasingController@view')->name('leasing_view');
-    Route::get('/leasing/select/download', 'BankController@index')->name('leasing_select_download');
-    Route::get('/leasing/pdf/download', 'BankController@downdload')->name('leasing_pdf_download');
-
-
-    // Route::get('/credit', 'DashboardController@credit')->name('dashboard_credit');
-    Route::get('/credit/view/{id}', 'CreditController@view')->name('credit_view');
-    Route::get('/credit/banks', 'CreditController@banks')->name('credit_banks');
-
-    Route::get('/news', 'NewsController@index')->name('news_list');
-    Route::get('/news/read/{id}', 'NewsController@index')->name('news_view');
-
-    Route::post('/callback', 'CallbackController@index')->name('callback_index');
-
-    Route::get('/configurator', 'ConfiguratorController@types')->name('configurator_index');
-    Route::get('/configurator/chassis/{alias}', 'ConfiguratorController@chassis')->name('configurator_chassis');
-    Route::get('/configurator/options/{typeId}/{baseId}', 'ConfiguratorController@options')->name('configurator_options');
-    Route::get('/configurator/leasing/{typeId}/{baseId}', 'ConfiguratorController@leasing')->name('configurator_leasing');
-    Route::get('/configurator/finish', 'ConfiguratorController@finish')->name('configurator_finish');
-
-    Route::get('/cart', 'CartController@index')->name('cart_index');
-
-    Route::get('search', 'SearchController@index')->name('search_index');
-    Route::get('/catalog/truck/{id}', 'SearchController@view')->name('search_view');
-
-
-    Route::get('/parts/view/{id}', 'PartsController@view')->name('parts_view');
-    Route::get('/parts/{alias}', 'PartsController@find')->name('parts_find');
-
-    Route::get('/articles/1', 'ArticlesController@index')->name('articles_index');
+    Route::get('/', 'DashboardController@parts')->name('dashboard_parts');
+    Route::get('/view/{id}', 'PartsController@view')->name('parts_view');
+    Route::get('/{alias}', 'PartsController@find')->name('parts_find');
 });
 
 Route::group(['middleware' => 'web', 'namespace' => 'Admin'], function() {
